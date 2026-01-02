dda314c174871b7e428ff66d9d766739bf4f24d0
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2026-01-02 15:04:14 -0600

    Revert "something is off with this test."
    This reverts commit c481abf7853e558fcb13e2c4d016fd7160a460dd.

--- test/pytest/test_app_manager.py
+++ test/pytest/test_app_manager.py
@@ -212,29 +212,29 @@ def test_pinging_app_from_outside_manager(qtbot, tmp_path):
     assert ret
 
 
-# def test_getting_values(qtbot, tmp_path):
-#     datadict = _make_testdata()
-#     datadict_to_hdf5(datadict, str(tmp_path), 'data')
+def test_getting_values(qtbot, tmp_path):
+    datadict = _make_testdata()
+    datadict_to_hdf5(datadict, str(tmp_path), 'data')
 
-#     appManager = AppManager()
-#     appManager.show()
-#     qtbot.waitExposed(appManager)
-#     qtbot.addWidget(appManager)
+    appManager = AppManager()
+    appManager.show()
+    qtbot.waitExposed(appManager)
+    qtbot.addWidget(appManager)
 
-#     assert appManager.launchApp(0, MODULE, FUNC, str(tmp_path), 'data')
+    assert appManager.launchApp(0, MODULE, FUNC, str(tmp_path), 'data')
 
-#     context = zmq.Context()
-#     socket = context.socket(zmq.REQ)
-#     socket.connect(f'tcp://127.0.0.1:12345')
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect(f'tcp://127.0.0.1:12345')
 
-#     socket.send_pyobj(tuple(["fc", 'getOutput', None]))
-#     reply = socket.recv_pyobj()
+    socket.send_pyobj(tuple(["fc", 'getOutput', None]))
+    reply = socket.recv_pyobj()
 
 
-#     ret = appManager.close()
-#     assert ret
+    ret = appManager.close()
+    assert ret
 
-#     assert reply is not None
+    assert reply is not None
 
 
 
